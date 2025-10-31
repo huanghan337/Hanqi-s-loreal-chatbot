b1ea5236cd59e1c890f6661d2c44ea72c2fe2984
huanghan337 <huanghan@iu.edu>
2025-10-31 20:01:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
 /* config.js - set WORKER_URL to your deployed Cloudflare Worker. */
 const WORKER_URL = "https://openaiapikey.hangqi39.workers.dev";
-const FRONTEND_SECRET = ""; // fill if you set FRONTEND_SECRET in Worker
